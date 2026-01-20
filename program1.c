e62ac2a8e4f0d3e04016f72742b5ca25401bd733
Ankita Mandal <ankitamandal.10h.2@gmail.com>
2026-01-20 19:32:36 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double true_value, measured_value,absolute_error, relative_error, percentage_error;
+
+    printf("Enter the true value: ");
+    scanf("%lf", &true_value);
+
+    printf("Enter the measured value: ");
+    scanf("%lf", &measured_value);
+
+    absolute_error = fabs(true_value - measured_value);
+    relative_error = absolute_error / true_value;
+    percentage_error = relative_error * 100;
+
+    printf("\nAbsolute Error = %lf", absolute_error);
+    printf("\nRelative Error = %lf", relative_error);
+    printf("\nPercentage Error = %lf %%", percentage_error);
+
+    return 0;
+}
